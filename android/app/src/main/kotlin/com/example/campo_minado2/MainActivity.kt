97452ac089386d646dd404ea9cb810586fc307a1
Aldo Vieira <aldovieir@hotmail.com>
2020-08-05 19:51:41 -0300

    project complete

---
+++ android/app/src/main/kotlin/com/example/campo_minado2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.campo_minado2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:campo_minado2/screens/campo_minado_app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(CampoMinadoApp());
